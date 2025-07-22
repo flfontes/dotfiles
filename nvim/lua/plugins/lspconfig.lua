95bf061a2cd621402814e7c35aa8eb212caba701
flfontes <98757275+flfontes@users.noreply.github.com>
2025-07-22 14:30:55 +0100

    update nvim configuration

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -182,7 +182,7 @@ return {
       -- But for many setups, the LSP (`ts_ls`) will work just fine
       -- ts_ls = {},
       --
-      basedpyright = {},
+      -- basedpyright = {},
       bashls = {},
       lua_ls = {
         -- cmd = { ... },
