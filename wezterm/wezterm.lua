e3ae14f36f2e69db58f95c551b52ca70158879e5
flfontes <98757275+flfontes@users.noreply.github.com>
2025-07-22 14:31:14 +0100

    add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,18 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+-- General Configuration
+
+config.default_cwd = "~/git"
+
+-- Look and Feel
+
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 11
+
+config.window_background_opacity = 0.9
+
+config.color_scheme = "Batman"
+
+return config
